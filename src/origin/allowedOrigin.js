35a8d6945a8e5115c764c8be72279c7c2c747cf5
[Ritik] <ritikkumar8077008067@gmail.com>
2025-11-05 14:55:46 +0530

    ag

--- src/origin/allowedOrigin.js
+++ src/origin/allowedOrigin.js
@@ -1,13 +1,11 @@
 export const allowedOrigins = [
     "http://localhost:5173",
     "https://shrijeefrontend.vercel.app/",
+    "https://shrijeefrontend.vercel.app",
     "https://shrijeefrontend-git-main-ritik-latiyans-projects.vercel.app/",
+    "https://shrijeefrontend-git-main-ritik-latiyans-projects.vercel.app",
     "https://shrijeefrontend-6untq2ea4-ritik-latiyans-projects.vercel.app/",
-    "https://shivikaichat.vercel.app/",
-    "https://shivikaichat.vercel.app",
-    "https://shivikaichat-git-main-ritik-latiyans-projects.vercel.app/",
-    "https://shivikaichat-git-main-ritik-latiyans-projects.vercel.app",
-    "https://shivikaichat-npt3esvtw-ritik-latiyans-projects.vercel.app/",
-    "https://shivikaichat-npt3esvtw-ritik-latiyans-projects.vercel.app",
+    "https://shrijeefrontend-6untq2ea4-ritik-latiyans-projects.vercel.app",
+ 
 
 ] ;
